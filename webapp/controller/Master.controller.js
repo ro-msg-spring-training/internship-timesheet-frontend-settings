5f68bcb7d2bbe06c995735ee9e8b4e3132038219
VadeanAndrada <andrada.vadean@gmail.com>
2020-05-19 17:16:07 +0000

    Story 9: #66 FE: filtering the programs (+ adding buttons)

--- webapp/controller/Master.controller.js
+++ webapp/controller/Master.controller.js
@@ -6,15 +6,19 @@ sap.ui.define([
 	"sap/ui/model/FilterOperator",
 	'sap/ui/model/Sorter',
 	'sap/m/MessageBox',
-	'sap/f/library'
-], function (Constants, JSONModel, Controller, Filter, FilterOperator, Sorter, MessageBox, fioriLibrary) {
+	'sap/f/library',
+	"sap/ui/core/format/DateFormat"
+], function (Constants, JSONModel, Controller, Filter, FilterOperator, Sorter, MessageBox, fioriLibrary, DateFormat) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.fiori2.controller.Master", {
 		onInit: function () {
 			this.oView = this.getView();
+			var oJSONData = {
+				count: 0
+			};
 
-			var oModel = new sap.ui.model.json.JSONModel();
+			var oModel = new sap.ui.model.json.JSONModel(oJSONData);
 
 			$.ajax({
 				type: "GET",
@@ -42,9 +46,47 @@ sap.ui.define([
 
 			oBinding.sort(oSorter);
 		},
+		// var oView = this.getView(),
+		// 		aStates = [undefined, "asc", "desc"],
+		// 		aStateTextIds = ["sortNone", "sortAscending", "sortDescending"],
+		// 		sMessage,
+		// 		iOrder = oView.getModel("appView").getProperty("/order");
 
-		onSynchronize: function (oEvent) {
-			this.onInit();
+		// 	// Cycle between the states
+		// 	iOrder = (iOrder + 1) % aStates.length;
+		// 	var sOrder = aStates[iOrder];
+
+		// 	oView.getModel("appView").setProperty("/order", iOrder);
+		// 	oView.byId("peopleList").getBinding("items").sort(sOrder && new Sorter("LastName", sOrder === "desc"));
+
+		// 	sMessage = this._getText("sortMessage", [this._getText(aStateTextIds[iOrder])]);
+
+		onFilter: function (oEvent) {
+			var iCount = this.oView.getModel("model").getProperty("/count");
+			var oTable = this.oView.byId("usersTable");
+			var oBinding = oTable.getBinding("items");
+			if (iCount % 2 == 0) {
+				var aFilter = [];
+				var oFormat = DateFormat.getDateInstance({
+					pattern: "yyyy-MM-dd"
+				});
+				var date = oFormat.format(new Date());
+				aFilter.push(new Filter({
+					path: "endDate",
+					operator: FilterOperator.GT,
+					value1: date
+				}));
+				oBinding.filter(aFilter);
+				this.oView.getModel("model").setProperty("/count", iCount + 1);
+				this.getView().byId("filterButton").setTooltip("Show all programs");
+				return;
+			} else {
+				oBinding.filter();
+				this.oView.getModel("model").setProperty("/count", iCount + 1);
+				this.getView().byId("filterButton").setTooltip("Show active programs");
+				return;
+			}
+
 		},
 
 		onPressed: function (oEvent) {
