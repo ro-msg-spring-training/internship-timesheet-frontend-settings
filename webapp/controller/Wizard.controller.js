02ae0c642296587e6271906303f3ccb338f0d0c5
ionas-emanuela <ema.ionas7@gmail.com>
2020-05-19 15:55:05 +0000

    Story 10 #68 FE: Create programs wizard (empty)

---
+++ webapp/controller/Wizard.controller.js
@@ -0,0 +1,51 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageBox"
+], function (Controller, JSONModel, MessageBox) {
+	"use strict";
+
+	return Controller.extend("sap.ui.demo.fiori2.controller.Wizard", {
+		onInit: function () {
+			this._wizard = this.byId("CreateProgramWizard");
+			this._oNavContainer = this.byId("wizardNavContainer");
+			this._oWizardContentPage = this.byId("wizardContentPage");
+
+		},
+
+		backToWizardContent: function () {
+			this._oNavContainer.backToPage(this._oWizardContentPage.getId());
+		},
+
+		_handleNavigationToStep: function (iStepNumber) {
+			var fnAfterNavigate = function () {
+				this._wizard.goToStep(this._wizard.getSteps()[iStepNumber]);
+				this._oNavContainer.detachAfterNavigate(fnAfterNavigate);
+			}.bind(this);
+
+			this._oNavContainer.attachAfterNavigate(fnAfterNavigate);
+			this.backToWizardContent();
+		},
+
+		_handleMessageBoxOpen: function (sMessage, sMessageBoxType) {
+			MessageBox[sMessageBoxType](sMessage, {
+				actions: [MessageBox.Action.YES, MessageBox.Action.NO],
+				onClose: function (oAction) {
+					if (oAction === MessageBox.Action.YES) {
+						this._handleNavigationToStep(0);
+						this._wizard.discardProgress(this._wizard.getSteps()[0]);
+					}
+				}.bind(this)
+			});
+		},
+
+		_setEmptyValue: function (sPath) {
+			this.model.setProperty(sPath, "n/a");
+		},
+
+		handleWizardCancel: function () {
+			this._handleMessageBoxOpen("Are you sure you want to cancel?", "warning");
+		}
+		
+	});
+});
